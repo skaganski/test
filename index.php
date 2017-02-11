62a50558cc6492a465ca94798e5fb7ef6836895a
Sergei Kaganski <skagansk@enos.itcollege.ee>
2017-02-11 16:08:37 +0200

    test

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php echo(phpversion());?>
+
+<head>
+<html>
+<title>First try</title>
+<link rel="stylesheet" type="text/css" href="MyFile.css">
+</head>
+<body>
+	<h1>Hello!</h1> 
+	<img src = "http://www.w3schools.com/css/trolltunga.jpg" widht="200" height="40">
+	
